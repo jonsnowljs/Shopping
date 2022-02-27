7daab9d3bb1e4401860456565d9518186ea5f9a2
Jiasheng Lu <1473875731@qq.com>
2022-02-27 09:43:24 -0700

    add footer and header to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
+import { Container } from 'react-bootstrap';
 import './App.css';
+import Footer from './components/Footer';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <>
+        <Header/>
+        <main className='py-3'>
+          <Container>
+            <h1>Hello Shoppi</h1>
+          </Container>
+        </main>
+        <Footer/>
+      </>
     </div>
   );
 }
