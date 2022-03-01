348aaa75719b5465890fdd45143a05c2246ce93f
Jiasheng Lu <1473875731@qq.com>
2022-03-01 00:21:28 -0700

    replace with react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,22 @@ import './App.css';
 import Footer from './components/Footer';
 import { Header } from './components/Header';
 import HomeScreen from './screens/HomeScreen';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
     <div className="App">
-      <>
-        <Header/>
-        <main className='py-3'>
-          <Container>
-            <HomeScreen/>
-          </Container>
-        </main>
-        <Footer/>
-      </>
+        <Router>
+          <Header />
+          <main className="py-3">
+            <Container>
+              <Route path="/" component={HomeScreen} exact />
+              <Route path="/product/:id" component={ProductScreen} />
+            </Container>
+          </main>
+          <Footer />
+        </Router>
     </div>
   );
 }
