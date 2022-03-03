3713cf4b916c27935154cd3301e9c2361a5c9890
Jiasheng Lu <1473875731@qq.com>
2022-03-03 01:13:49 -0700

    Update HomeScreen.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,21 +1,36 @@
-import React from 'react'
-import products from 'assets/data/products'
-import { Row, Col } from 'react-bootstrap';
+import React, {useEffect, useState} from 'react'
 import Product from '../components/Product'
+import { Row, Col } from 'react-bootstrap';
+import axios from 'axios';
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+      axios.get('/api/products').then((res) => setProducts(res.data));
+  },[])
+  // const fetchProducts = async() => {
+  //   const {data} = await axios.get('/api/products')
+  //   setProducts(data)
+
+  // }
+  // fetchProducts()
+  
+
+
   return (
     <>
       <h1>Latest Products</h1>
+      <h1>{products}</h1>
       <Row>
         {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product}/>
+            <Product product={product} />
           </Col>
         ))}
       </Row>
     </>
-  )
+  );
 }
 
 export default HomeScreen
