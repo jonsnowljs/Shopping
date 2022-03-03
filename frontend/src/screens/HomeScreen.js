e4251146b6af5d5a524474771228bebeaad04fa7
Jiasheng Lu <1473875731@qq.com>
2022-03-03 01:27:56 -0700

    move product from frontend to backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,24 +4,15 @@ import { Row, Col } from 'react-bootstrap';
 import axios from 'axios';
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
       axios.get('/api/products').then((res) => setProducts(res.data));
   },[])
-  // const fetchProducts = async() => {
-  //   const {data} = await axios.get('/api/products')
-  //   setProducts(data)
-
-  // }
-  // fetchProducts()
-  
-
 
   return (
     <>
       <h1>Latest Products</h1>
-      <h1>{products}</h1>
       <Row>
         {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../assets/data/products';
+import products from '../assets/products';
 
 const ProductScreen = ({ match }) => {
   const product = products.find((p) => p._id === match.params.id);
