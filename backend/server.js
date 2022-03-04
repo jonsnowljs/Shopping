34e0b336149d7d7294b5d51194e063c8157e5a68
Jiasheng Lu <1473875731@qq.com>
2022-03-04 00:10:57 -0700

    connect with mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 import express from'express'
-import products from './data/products.js'
 import dotenv from 'dotenv'
+import colors from 'colors'
+import products from './data/products.js'
+import connectDB from './config/db.js'
+
 
 dotenv.config()
 
+connectDB()
+
 const app = express()
+
+app.get('/', (req, res) => {
+  res.send('API is running...')
+})
 
 app.get('/api/products', (req, res) => {
   res.json(products)
@@ -17,5 +26,5 @@ app.get('/api/products/:id', (req, res) => {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
 
