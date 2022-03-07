2adf443c6066fb6038ee69e5f9776cbccda9754b
Jiasheng Lu <1473875731@qq.com>
2022-03-06 20:40:51 -0700

    add backend router

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
-import products from './data/products.js'
 import connectDB from './config/db.js'
+
+import productRoutes from './routes/productRoutes.js'
 
 
 dotenv.config()
@@ -15,6 +16,8 @@ app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
+
+app.use('/api/products', productRoutes)
 app.get('/api/products', (req, res) => {
   res.json(products)
 })
