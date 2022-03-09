e1230ba0600b87b81fb512fdf3cc7f6b4385d7b3
Jiasheng Lu <1473875731@qq.com>
2022-03-09 00:29:43 -0700

    Add Loader and alert to product detail page

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,15 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { productListReducer } from './reducers/productReducers';
+import {
+  productListReducer,
+  productDetailsReducer,
+} from './reducers/productReducers';
 
 // TODO replace redux-devtools-extension to redux-devtools/core
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 });
 
 const initialState = {};
