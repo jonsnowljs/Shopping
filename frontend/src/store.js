7f875f0e6afec93b43cbd47b88c3600099283490
Jiasheng Lu <1473875731@qq.com>
2022-03-11 00:42:27 -0700

    put user info info to redux

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,26 +1,33 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+
 import {
   productListReducer,
   productDetailsReducer,
 } from './reducers/productReducers';
-
 import { cartReducer } from './reducers/cartReducers';
-
+import { userLoginReducer } from './reducers/userReducers';
 // TODO replace redux-devtools-extension to redux-devtools/core
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
+// put cartItems to initialState when cartItems is updated
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : [];
 
+// put user info to initialState when userinfo is updated
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('uerInfo'))
+  : [];
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 };
 
 const middleware = [thunk];
