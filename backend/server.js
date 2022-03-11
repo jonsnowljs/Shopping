9902ecbc251163a98ca8c90ddd02717f805c3553
Jiasheng Lu <1473875731@qq.com>
2022-03-10 23:38:22 -0700

    add token to user authentication

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,38 @@
-import express from'express'
-import dotenv from 'dotenv'
-import colors from 'colors'
-import connectDB from './config/db.js'
-import {notFound, errorHandler} from './middleware/errorMiddleware.js'
+import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
-import productRoutes from './routes/productRoutes.js'
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
+dotenv.config();
 
-dotenv.config()
+connectDB();
 
-connectDB()
+const app = express();
 
-const app = express()
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  console.log("Home")
-  res.send('API is running...')
-})
+  console.log('Home');
+  res.send('API is running...');
+});
 
-
-app.use('/api/products', productRoutes)
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 // when the address don't meet / and /api/products, it will call the app.use(notFound), app.use(notFound) will pass the error to errorHandler
 app.use(notFound);
 // errorHandler will handle the error throw by productRouter.js and error passed by notFound
-app.use(errorHandler)
+app.use(errorHandler);
 
+const PORT = process.env.PORT || 5000;
 
-
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
-
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
