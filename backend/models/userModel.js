aae67d0daf8a61a9df050a3e45bf4a15e06f2833
Jiasheng Lu <1473875731@qq.com>
2022-03-11 00:15:11 -0700

    add user register controller

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -30,5 +30,15 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+// hash the password before save
+userSchema.pre('save', async function (next) {
+  // check is the password is modified to avoid hash hashed password
+  if (!this.isModified('password')) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model('User', userSchema);
 export default User;
