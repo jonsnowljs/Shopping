4513384b0d715845361cd13f6f64d8bff9e38a0e
Jiasheng Lu <1473875731@qq.com>
2022-03-15 20:47:13 -0600

    Add place order screen

--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -29,26 +29,27 @@ const PaymentScreen = ({ history }) => {
       <Form onSubmit={submitHandler}>
         <Form.Group>
           <Form.Label as="legend">Select Method</Form.Label>
+          <Col>
+            <Form.Check
+              type="radio"
+              label="PayPal or Credit Card"
+              id="PayPal"
+              name="paymentMethod"
+              value="PayPal"
+              checked
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></Form.Check>
+            <Form.Check
+              type="radio"
+              label="Stripe"
+              id="Stripe"
+              name="paymentMethod"
+              value="Stripe"
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></Form.Check>
+          </Col>
         </Form.Group>
-        <Col>
-          <Form.Check
-            type="radio"
-            label="PayPal or Credit Card"
-            id="PayPal"
-            name="paymentMethod"
-            value="PayPal"
-            checked
-            onChange={(e) => setPaymentMethod(e.target.value)}
-          ></Form.Check>
-          <Form.Check
-            type="radio"
-            label="Stripe"
-            id="Stripe"
-            name="paymentMethod"
-            value="Stripe"
-            onChange={(e) => setPaymentMethod(e.target.value)}
-          ></Form.Check>
-        </Col>
+
         <Button type="submit" variant="primary">
           Continue
         </Button>
