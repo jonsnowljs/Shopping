c33e4ba8278067cec93a830c0bf27ecd92bc10e4
Jiasheng Lu <1473875731@qq.com>
2022-03-21 23:31:40 -0600

    add admin check middleware and get users api

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,10 +5,12 @@ import {
   getUserProfile,
   registerUser,
   updateUserProfile,
+  getUsers,
 } from '../controllers/userController.js';
-import { protect } from '../middleware/authMiddleware.js';
+import { admin, protect } from '../middleware/authMiddleware.js';
 
-router.route('/').post(registerUser);
+// /api/users
+router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.post('/login', authUser);
 router
   .route('/profile')
